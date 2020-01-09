1340fc641af4169452fab7c8befe1d6a3119808d
Swedish <tarthurf@yahoo.com>
2020-01-08 21:45:51 -0800

    created objects to hold check box booleans

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Slider functionality
-var slider = document.getElementById("charRange");
-var output = document.getElementById("sliderValue");
+let slider = document.getElementById("charRange");
+let output = document.getElementById("sliderValue");
 output.innerHTML = slider.value;
 
 // Slider output on html
@@ -11,7 +11,7 @@ slider.oninput = function() {
 // Arrays for letters and symbols
 let letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-let symbol = [" ", ",", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "", "^", "`", "{", "|", "}", "~", "[", "]"];
+let symbol = [" ", ",", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "`", "{", "|", "}", "~", "[", "]"];
 
 // Prompt for what charcters the user wants
 // This will change to a less intrusive method later, used mainly for testing at this point
@@ -31,3 +31,38 @@ if (test === 1) {
     let useSymbol = confirm("Would you like to use symbols?");
 }
 
+// Checkbox objects
+let alpha = document.getElementById("letterCheck");
+let caps = document.getElementById("capsCheck");
+let numb = document.getElementById("numberCheck");
+let symbols = document.getElementById("symbolCheck");
+
+function noCap() {
+    console.log(`caps ${caps.checked} \nalpha ${alpha.checked}`);
+}
+
+// create generate button dom element
+if (test === 1) {
+    function noCap() {
+        if (alpha.checked === false && caps.checked === true) {
+            console.log(`caps = ${caps.checked} \nalpha = ${alpha.checked}`);
+            alert("You cannot capitalize letters that don't exist!");
+
+        }
+    }
+}
+
+
+// check range and which parameters are enabled
+function arrayLoop(arrayName) {
+    let y = arrayName;
+    for (let i = 0; i < y.length; i++) {
+        console.log(y[i]);
+    }
+}
+
+arrayLoop(letter)
+arrayLoop(symbol)
+// Generate numbers function
+
+// generate password
