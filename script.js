c60c68bc04b242628056bb9930b12f3bbdc687fe
Swedish <tarthurf@yahoo.com>
2020-01-10 21:01:40 -0800

    Added a valSelcet function that loops through an array

--- script.js
+++ script.js
@@ -16,27 +16,55 @@ let letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "
 
 let symbol = [" ", ",", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "`", "{", "|", "}", "~", "[", "]"];
 
+let password = [];
+
+let chance = 0
+
 // Checkbox objects
 let alpha = document.getElementById("letterCheck");
 let caps = document.getElementById("capsCheck");
 let numb = document.getElementById("numberCheck");
 let symbols = document.getElementById("symbolCheck");
 
-if (test === 1) {
-    function noCap() {
-        console.log(`caps ${caps.checked} \nalpha ${alpha.checked}`);
-    }
-}
 // create generate button dom element
-if (test === 1) {
+if (test === 2) {
     function noCap() {
         if (alpha.checked === false && caps.checked === true) {
             console.log(`caps = ${caps.checked} \nalpha = ${alpha.checked}`);
             alert("You cannot capitalize letters that don't exist!");
-
+        } else {
+            generate()
         }
     }
 }
+
+// This function will generate a random number
+function valSelect(array) {
+    for (let i = 0; i < array.length; i++) {
+        
+    }
+}            
+
+let chance = Math.random();
+
+function generate() {
+    for (let i = 0; i < output.length; i++) {
+        if (chance < .25) {
+            if (letterCheck === true) {
+                password.pop()
+            }
+        } else if (chance > .25 && chance < .50) {
+            
+        } else if (chance > .50 && chance < .75) {
+            
+        } else if (chance > .75 && chance < .99) {
+            
+        }        
+    
+
+    }
+}
+
 
 
 // check range and which parameters are enabled
@@ -45,6 +73,13 @@ function arrayLoop(arrayName) {
     for (let i = 0; i < y.length; i++) {
         console.log(y[i]);
     }
+}
+
+function generate() {
+    for (let i = 0; i < output.length; i++) {
+        
+    }
+    
 }
 
 arrayLoop(letter)
